93c16812a13f06fb00e3ca1cb03a39f7f2b5819c
Letícia Nunes <leticiavuzz@gmail.com>
2026-02-15 21:17:13 -0300

    Create valoresliterais.cs

---
+++ valoresliterais.cs
@@ -0,0 +1,7 @@
+Console.WriteLine ('b');
+Console.WriteLine (123);
+Console.WriteLine (0.25F);
+Console.WriteLine (2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
